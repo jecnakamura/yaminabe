42ae2cca35444d28f69549369b94982b786a7974
jecci5 <billycheung1997@gmail.com>
2024-11-14 13:45:10 +0900

    deta(kari)

---
+++ Assets/Fujisaki/Test.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int ID;
+    public string name;
+    public string janru;
+    public int itemscore;
+   // public double aisyou;
+}

72048fbfdee9c05f74d9c08bca8a8b020f6e1eae
jecci5 <billycheung1997@gmail.com>
2024-11-14 14:36:28 +0900

    kari

--- Assets/Fujisaki/Test.cs
+++ Assets/Fujisaki/Test.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+[System.Serializable]
+public class Test
 {
     public int ID;
     public string name;

b6b557698c4ffcd1f2fc0427daf57cdd718d42ee
dteam <22ci0515@jec.ac.jp>
2024-11-20 16:40:25 +0900

    akieckdjoeadfaaaaaaaaaaaaaaaaaaasdfaseaaaaaaaaaaaaaaaaaaaaa

--- Assets/Yoshida/Script/RoulettePiece.cs
+++ Assets/Yoshida/Script/RoulettePiece.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoulettePiece : MonoBehaviour
 {
     public int No;
+    public GameObject Me;
+    private Text textComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Me = this.gameObject;
+        textComponent = GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame

f2071b7fc7c794b25bfcd45395fd7148fc9e1f34
CI <CI@jec.ac.jp>
2024-11-21 12:21:18 +0900

    キャラクター選択とNPCの強さ選択が出来るようにしたよ

--- Assets/Kouda/Script/CharacterData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewCharacter", menuName = "Character")]
-public class CharacterData : ScriptableObject
-{
-    public string characterName;
-    public Sprite characterImage;
-}

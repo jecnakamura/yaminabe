f05fe1adcf85c7dbbc3cbf25ef0d9ac2cc017222
CI <CI@jec.ac.jp>
2024-11-21 15:39:13 +0900

    no message

---
+++ Assets/Kouda/Script/RuleSceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RuleSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // ƒ‹[ƒ‹à–¾‰æ–Ê‚Ö‘JˆÚ
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+}

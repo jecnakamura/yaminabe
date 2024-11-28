a93b26632910e838798851f0cb5b298c8ad37127
jecci5 <billycheung1997@gmail.com>
2024-11-28 11:49:15 +0900

    ルーレットが時間経過で消えるように

--- Assets/Fujisaki/RurettoMane.cs
+++ Assets/Fujisaki/RurettoMane.cs
@@ -5,15 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class RurettoMane : MonoBehaviour
 {
+    [SerializeField]
+    private float seconds;
+    private bool kari;
     // Start is called before the first frame update
     void Start()
     {
-        
+        seconds = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            kari = true;
+        }
+        if(kari == true) {
+            seconds += Time.deltaTime;
+            if (seconds >= 6f)
+            {
+                SceneManager.UnloadSceneAsync("Ruretto");
+            }
+        }
         
     }
     public void OnClick()

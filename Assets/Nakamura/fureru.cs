9f8bf7f27ba1580be38d39385698ec7056ede098
Morita <22ci0244@jec.ac.jp>
2024-12-12 16:40:11 +0900

    no message

--- Assets/Nakamura/fureru.cs
+++ Assets/Nakamura/fureru.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class fureru : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hit"); // ÉçÉOÇï\é¶Ç∑ÇÈ
     }
 }

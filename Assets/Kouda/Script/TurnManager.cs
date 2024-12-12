68a16316fa243f3059ee46622d7f3865c2408e2e
CI <CI@jec.ac.jp>
2024-12-12 13:48:23 +0900

    no message

--- Assets/Kouda/Script/TurnManager.cs
+++ Assets/Kouda/Script/TurnManager.cs
@@ -28,7 +28,7 @@ public class TurnManager : MonoBehaviour
     private int currentPlayerIndex = 0;
     public MapManager mapManager; // マップ管理クラス
     public CameraController cameraController;   //カメラ管理クラス
-    //public GameUIManager uiManager; // UI管理クラス
+    public UIManager uiManager; // UI管理クラス
 
     public List<GameObject> commandButtons;
 
@@ -180,7 +180,7 @@ public class TurnManager : MonoBehaviour
     private IEnumerator HandlePlayerTurn(Player player)
     {
         // UIでターン情報を表示
-        //uiManager.ShowTurnInfo(player);
+        uiManager.ShowTurnInfo(player);
 
         // マスの移動処理
         for (int i = 0; i < player.MoveSteps; i++)

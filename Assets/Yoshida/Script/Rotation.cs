d7f36868d1ce728fe58def5d0060db858bfc4050
jecci5 <billycheung1997@gmail.com>
2025-01-23 14:54:08 +0900

    no message

--- Assets/Yoshida/Script/Rotation.cs
+++ Assets/Yoshida/Script/Rotation.cs
@@ -10,13 +10,18 @@ public class Rotation : MonoBehaviour
     public float DecelerationMin;
     public float DecelerationMax;
     public float MinSpeed;
+    public float DecelerationFactor = 0.7f;
 
     public Vector3 RayGoal;
     public Button startButton;
+    public Button stopButton;
+
+    private bool isDecelerating = false;
     public enum State
     {
         Standby,
         Rotate,
+        Decelerating,
     }
     State state = State.Standby;
 
@@ -31,6 +36,10 @@ public class Rotation : MonoBehaviour
         {
             startButton.onClick.AddListener(OnStartButtonClicked);
         }
+        if (stopButton != null)
+        {
+            stopButton.onClick.AddListener(OnStopButtonClicked);
+        }
     }
     private void OnStartButtonClicked()
     {
@@ -40,26 +49,35 @@ public class Rotation : MonoBehaviour
             state = State.Rotate;
         }
     }
+    private void OnStopButtonClicked()
+    {
+        isDecelerating = true;
+        state = State.Decelerating;
+    }
     // Update is called once per frame
     public void Update()
     {
         switch (state)
         {
             case State.Standby:
-                //if (Input.GetKeyDown(KeyCode.Space))
-                //{
-                //    this.rotSpeed = Random.Range(FirstSpeedMin, FirstSpeedMax);
-                //    state = State.Rotate;
-                //}
                 break;
 
             case State.Rotate:
-                this.rotSpeed *= Random.Range(DecelerationMin, DecelerationMax);
                 transform.Rotate(0, 0, this.rotSpeed);
+                break;
+            case State.Decelerating:
                 if (rotSpeed < MinSpeed)
                 {
+                    // 減速を適用して回転させる
+                    rotSpeed *= DecelerationFactor;
+                    transform.Rotate(0, 0, this.rotSpeed);
+                }
+                else
+                {
+                    // 最小速度に達したら停止
                     rotSpeed = 0;
 
+                    // ヒット判定をして結果を表示
                     var goal = Camera.main.WorldToScreenPoint(RayGoal);
                     Ray ray = Camera.main.ScreenPointToRay(goal);
                     int layerMask = 0xffff;
@@ -72,11 +90,10 @@ public class Rotation : MonoBehaviour
                         RouletteResultHandler.SetEnd(true);
                         Debug.Log(piece.No);
                     }
+
                     state = State.Standby;
                 }
                 break;
-
-
         }
     }
 

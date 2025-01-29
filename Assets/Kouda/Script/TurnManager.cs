d0f9ae548db86c46d6922fafadb55d85f5ea0368
dteam <22ci0515@jec.ac.jp>
2025-01-29 11:46:39 +0900

    no message

--- Assets/Kouda/Script/TurnManager.cs
+++ Assets/Kouda/Script/TurnManager.cs
@@ -95,6 +95,7 @@ public class TurnManager : MonoBehaviour
 
             players.Add(player);
             player.SetCharaImage();
+
         }
     }
 
@@ -141,6 +142,13 @@ public class TurnManager : MonoBehaviour
     private IEnumerator HandleState(Player currentPlayer)
     {
         playerInventory.Inventory(currentPlayer);
+
+        foreach (var p in players)
+        {
+            p.display.sortingOrder = 5;
+        }
+        currentPlayer.display.sortingOrder = 6;
+
         switch (state)
         {
             case TurnState.CommandSelect:
